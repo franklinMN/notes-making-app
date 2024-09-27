8720a8566a26f6da733ac4e9ff020a29d4c2a065
franklinMN <frankmnwinter@gmail.com>
2024-09-27 15:35:05 +0530

    added bootstrap and fontawesome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Header.css";
+import noteslogo from "../assets/noteslogo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="menubox">
+        <i class="fa-solid fa-bars fa-2x"></i>
+      </div>
+      <div className="logobox">
+        <img src={noteslogo} alt="notes logo" />
+        <h2>Notes</h2>
+      </div>
+      <div className="searchbox">searchbox</div>
+      <div className="userbox">userbox</div>
+    </div>
+  );
 };
 
 export default Header;
